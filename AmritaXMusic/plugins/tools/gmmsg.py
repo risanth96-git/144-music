2edfb575d58e869cb9be671e120275bd4c9ea95d
THE ARCHITECT <markusmarwin00@gmail.com>
2024-12-27 12:17:32 +0530

    Create gmmsg.py

---
+++ AmritaXMusic/plugins/tools/gmmsg.py
@@ -0,0 +1,36 @@
+import re
+from pyrogram import filters
+import random
+from SONALI import app
+
+@app.on_message(filters.command(["gm", "goodmorning", "good morning"], prefixes=["/", "g", "G"]))
+async def goodmorning_command_handler(_, message):
+    sender = message.from_user.mention
+    send_sticker = random.choice([True, False])
+    
+    if send_sticker:
+        sticker_id = get_random_sticker()
+        await app.send_sticker(message.chat.id, sticker_id)  # Await the async call
+        await message.reply_text(f"Good morning, {sender}! Have a wonderful day! ☀️")  # Await the async call
+    else:
+        emoji = get_random_emoji()
+        await app.send_message(message.chat.id, emoji)  # Await the async call
+        await message.reply_text(f"Good morning, {sender}! Have a wonderful day! {emoji}")  # Await the async call
+
+def get_random_sticker():
+    stickers = [
+        "CAACAgQAAx0Ce9_hCAACaEVlwn7HeZhgwyVfKHc3WUGC_447IAACLgwAAkQwKVPtub8VAR018x4E",  # Sticker 1
+        "CAACAgIAAx0Ce9_hCAACaEplwn7dvj7G0-a1v3wlbN281RMX2QACUgwAAligOUoi7DhLVTsNsh4E",  # Sticker 2
+        "CAACAgIAAx0Ce9_hCAACaFBlwn8AAZNB9mOUvz5oAyM7CT-5pjAAAtEKAALa7NhLvbTGyDLbe1IeBA",  # Sticker 3
+        "CAACAgUAAx0CcmOuMwACldVlwn9ZHHF2-S-CuMSYabwwtVGC3AACOAkAAoqR2VYDjyK6OOr_Px4E",
+        "CAACAgIAAx0Ce9_hCAACaFVlwn-fG58GKoEmmZpVovxEj4PodAACfwwAAqozQUrt2xSTf5Ac4h4E",
+    ]
+    return random.choice(stickers)
+
+def get_random_emoji():
+    emojis = [
+        "☀️",
+        "🌞",
+        "🌅",
+    ]
+    return random.choice(emojis)

27d35bb3a489e04fe95aa54efc9832cceefeece3
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-12-28 00:33:09 +0530

    Update couples.py

--- AmritaXMusic/plugins/tools/couples.py
+++ AmritaXMusic/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from AmritaXMusic.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="𝁘𝃜𝆆♡〭𝀈 ᴀᴅᴅ ᴍᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ 𝁘𝃜𝆆♡〭𝀈",
-            url=f"https://t.me/DenkiMusicBot?startgroup=true",
+            text="𓆰⚡͚̍𝆹𝅥𝐁𝔢𝖆𝛅𝐓 𝐅🐺𝐗 𝐍𝔢𝖙𝔴𝖔𝔯𝐊🕸️",
+            url=f"https://t.me/beast_fox_network",
         ),
     ],
 ]
